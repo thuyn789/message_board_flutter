3000d87093a60494b4c4a298106056c363bbc505
thuyn789 <thuynh0135@gmail.com>
2021-06-28 01:14:27 -0400

    Set up Firebase SDK

---
+++ lib/user.dart
@@ -0,0 +1,11 @@
+class UserIntance {
+  final String firstName;
+  final String lastName;
+  final String email;
+
+  UserIntance({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+  });
+}
